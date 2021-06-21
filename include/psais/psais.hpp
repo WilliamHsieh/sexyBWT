3e1af6f8fc34678448e5058a82e79daef0e625b0
marmot0814 <s1357andy871207@gmail.com>
2021-06-21 18:23:23 +0800

    [FEAT] get_lms parallelism

--- include/psais/psais.hpp
+++ include/psais/psais.hpp
@@ -210,11 +210,36 @@ auto get_bucket(const std::vector<CharType> &S, IndexType K) {
 // ##get_lms
 template<typename IndexType>
 auto get_lms(const std::vector<uint8_t> &T) {
-	auto LMS = std::vector<IndexType>{};
-	for (size_t i = 0; i < T.size(); i++) {
-		if (is_LMS(T, i)) LMS.push_back(i);
-	}
-	return LMS;
+	IndexType n = T.size();
+	std::vector<std::vector<IndexType>> stk(NUM_THREADS);
+	for (int i = 0; i < NUM_THREADS; i++)
+		stk.reserve(n / NUM_THREADS + 1);
+
+	psais::utility::parallel_do (
+		n, NUM_THREADS, [&](
+			IndexType L, IndexType R, int tid
+		) {
+			for (IndexType i = L; i < R; i++)
+				if (i != 0 and T[i - 1] == L_TYPE and T[i] == S_TYPE)
+					stk[tid].push_back(i);
+		}
+	);
+
+	std::vector<IndexType> prefix_sum(NUM_THREADS + 1, 0);
+	for (int i = 0; i < NUM_THREADS; i++)
+		prefix_sum[i + 1] = prefix_sum[i] + (IndexType)stk[i].size();
+
+	std::vector<IndexType> lms(prefix_sum.back());
+
+	psais::utility::parallel_do (
+		n, NUM_THREADS, [&](
+			IndexType L, IndexType R, int tid
+		) {
+			for (IndexType i = 0; i < (IndexType)stk[tid].size(); i++)
+				lms[prefix_sum[tid] + i] = stk[tid][i];
+		}
+	);
+	return lms;
 }
 
 // #suffix_array

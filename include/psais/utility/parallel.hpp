299ac1fc3df10b22c7c25f2c957e0ad725113852
williamhsieh <wh31110@gmail.com>
2021-06-22 12:44:39 +0800

    [FEAT] parallel_init

--- include/psais/utility/parallel.hpp
+++ include/psais/utility/parallel.hpp
@@ -4,6 +4,12 @@
 
 namespace psais::utility {
 
+template<typename T>
+std::reference_wrapper<std::remove_reference_t<T>> wrapper(T& t) { return std::ref(t); }
+
+template<typename T>
+T&& wrapper(std::remove_reference_t<T>&& t) { return std::move(t); }
+
 template <typename Func, typename ... Args>
 void parallel_do (
 	std::integral auto n_jobs,
@@ -21,9 +27,25 @@ void parallel_do (
 		if (block_size == 0) break;
 
 		auto R = std::min(n_jobs, L + block_size);
-		threads.emplace_back(std::forward<Func>(func), L, R, tid, std::forward<Args>(args)...);
+		threads.emplace_back(std::forward<Func>(func), L, R, tid, wrapper<Args>(args)...);
 		L = R;
 	}
+}
+
+template <typename Vec, typename Val>
+void parallel_init (
+	std::integral auto n_jobs,
+	std::integral auto n_threads,
+	Vec&& container,
+	Val&& value
+) {
+	parallel_do(n_jobs, n_threads,
+		[](auto L, auto R, auto, Vec& v, auto x) {
+			for (auto i = L; i < R; i++) {
+				v[i] = x;
+			}
+		}, std::forward<Vec>(container), std::forward<Val>(value)
+	);
 }
 
 } //namespace psais::utility

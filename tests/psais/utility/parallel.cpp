6c3feff4aac4a9fb2a2cef9659fcf5116bfe0f4f
williamhsieh <wh31110@gmail.com>
2021-06-22 12:45:01 +0800

    [TEST] parallel_init

--- tests/psais/utility/parallel.cpp
+++ tests/psais/utility/parallel.cpp
@@ -12,20 +12,16 @@ int main() {
 		auto beg = std::chrono::high_resolution_clock::now();
 
 		// initialize
-		psais::utility::parallel_do (
-			n_jobs, n_threads, [](int L, int R, int tid, std::vector<int> &arr) {
-			for (int i = L; i < R; i++) {
-				arr[i] = 5;
-			}
-		}, std::ref(v));
+		psais::utility::parallel_init(n_jobs, n_threads, v, 5);
 
 		// process
-		psais::utility::parallel_do (
-			n_jobs, n_threads, [](int L, int R, int tid, std::vector<int> &arr, int x, int y) {
-			for (int i = L; i < R; i++) {
-				arr[i] += x * y;
-			}
-		}, std::ref(v), 5, 10);
+		psais::utility::parallel_do(n_jobs, n_threads,
+			[&v](int L, int R, int tid, std::vector<int> &arr, int x, int y) {
+				for (int i = L; i < R; i++) {
+					arr[i] += x * y + tid * 0;
+				}
+			}, v, 5, 10
+		);
 
 		auto end = std::chrono::high_resolution_clock::now();
 		std::cout << std::setw(3) << n_threads << " threads: " << (end - beg).count() << '\n';

4db6a8d5834fe925a804959bc5eff0dedd42aa6f
williamhsieh <wh31110@gmail.com>
2021-06-27 00:10:10 +0800

    [TEST] rewrite thread_pool unit test

--- tests/psais/utility/thread_pool.cpp
+++ tests/psais/utility/thread_pool.cpp
@@ -1,59 +1,26 @@
-#include <iostream>
-#include <future>
-#include <random>
-#include <vector>
 #include "psais/utility/thread_pool.hpp"
 
-std::atomic<int> num_job1 = 0;
-std::condition_variable cv;
-std::mutex cv_m;
+struct Uncopyable {
+	Uncopyable() = default;
+	Uncopyable(const Uncopyable&) = delete;
 
-void print_1() {
-	if (--num_job1 == 0) {
-		cv.notify_all();
-	}
-}
-
-struct print_2 {
-	void operator()() {
-		std::unique_lock<std::mutex> lock(cv_m);
-		cv.wait(lock, []{ return num_job1 == 0; });
-		lock.unlock();
+	int operator()(int n) {
+		if (n <= 1) return n;
+		return operator()(n - 1) + operator()(n - 2);
 	}
 };
 
-int fib(int n) {
-	if (n <= 1) return n;
-	return fib(n - 1) + fib(n - 2);
-}
-
 TEST(utility, thread_pool) {
 	auto pool = psais::utility::ThreadPool(5);
-	auto result = std::vector<std::future<void>>{};
 
-	// send jobs
-	for (int i = 0; i < 8; i++, num_job1++) {
-		result.push_back(pool.enqueue(print_1));
-	}
-	for (int i = 0; i < 4; i++) {
-		result.push_back(pool.enqueue(print_2{}));
-	}
-
-	// get result from future
-	for (auto& r : result) {
-		r.get();
-	}
-	ASSERT_EQ(num_job1, 0);
-
-	// different return type(int)
-	auto res = pool.enqueue(fib, 30);
-	res.wait();
+	// function / functor
+	Uncopyable fib;
+	auto res1 = pool.enqueue(std::ref(fib), 40);
 
 	// lambda
 	auto lam = [](int x, int y) { return x + y; };
 	auto res2 = pool.enqueue(lam, 3, 5);
-	res2.wait();
 
-	ASSERT_EQ(res.get(), fib(30));
+	ASSERT_EQ(res1.get(), Uncopyable{}(40));
 	ASSERT_EQ(res2.get(), 8);
 }
